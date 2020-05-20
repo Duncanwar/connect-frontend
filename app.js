293789039a85603a40073ad3a6d17cdc73a6637f
Duncanwar <semunda9@gmail.com>
2020-05-20 17:08:39 +0200

    post route

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const mongoose = require("mongoose")
 const {MONGOURI} = require('./keys')
 const bodyParser = require('body-parser');
 
-
-
 mongoose.connect(MONGOURI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -20,9 +18,12 @@ mongoose.connection.on('error',(err)=>{
 })
 
 require('./models/user')
+require('./models/post')
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
 
 app.listen(PORT,()=>{
     console.log("server is running on,PORT")
