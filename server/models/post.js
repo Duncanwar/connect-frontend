aaf57bf7975ddf78d86d779d89932695b097c8fc
Duncanwar <semunda9@gmail.com>
2020-05-25 21:22:58 +0200

    likes and unlikes

--- server/models/post.js
+++ server/models/post.js
@@ -13,6 +13,8 @@ const postSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    likes:[{type:ObjectId,
+    ref:"user"}],
     postedBy:{
         type:ObjectId,
         ref:"user"
