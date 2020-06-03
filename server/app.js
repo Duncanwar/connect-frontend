24993f2e0f829948da6ab94e310b7d4069048ab5
Duncanwar <semunda9@gmail.com>
2020-06-03 10:20:18 +0200

    changes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const mongoose = require("mongoose")
 const {MONGOURI} = require('./config/keys')
 const bodyParser = require('body-parser');
@@ -25,6 +25,14 @@ app.use(bodyParser.json());
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(_dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log("server is running on,PORT",PORT)
