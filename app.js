81234c04b7c7d24d0a3b4cd814e9f254ff211002
Duncanwar <semunda9@gmail.com>
2020-09-14 14:25:37 +0200

    done

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
     const path = require('path')
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname+'client/build/index.html'))
