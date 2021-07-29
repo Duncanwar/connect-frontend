96efc1c3383aa2873d5b441d35f0d5bf17134c69
Duncanwar <semunda9@gmail.com>
2021-07-29 13:16:36 +0200

    instagram clone changed to general social media

--- routes/bookmark.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router  = express.Router();
-const requireLogin = require('../middleware/requireLogin')
-import BookmarkController from "../controllers/bookmark.controller";
-
-router.post('/bookmark', requireLogin, BookmarkController.add);
-router.get('/bookmark', requireLogin, BookmarkController.getBookMarkedPostByUserId)
-
-module.exports = router;
