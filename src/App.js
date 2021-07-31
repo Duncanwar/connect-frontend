5186087687e8e5cedf8f043ec1f87bf3ea9ab291
Duncanwar <semunda9@gmail.com>
2021-07-31 23:58:30 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,90 +1,92 @@
-import React ,{useEffect,createContext,useReducer,useContext}from 'react';
-import './App.css';
-import Nav from './components/Navbar'
-import {BrowserRouter,Route, Switch, useHistory} from 'react-router-dom'
-import Home from './components/screens/Home'
-import Signin from './components/screens/Login'
-import Signup from './components/screens/Signup'
-import Profile from './components/screens/Profile'
-import CreatePost from './components/screens/CreatePost'
-import {reducer,initialState} from './reducers/userReducer'
-import UserProfile from './components/screens/UserProfile'
-import UserSubscribe from './components/screens/UserSubscribe'
-import Reset from "./components/screens/Reset"
+import React, { useEffect, createContext, useReducer, useContext } from "react";
+import "./App.css";
+import Nav from "./components/Navbar";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
+import Home from "./components/screens/Home";
+import Signin from "./components/screens/Login";
+import Signup from "./components/screens/Signup";
+import Profile from "./components/screens/Profile";
+import CreatePost from "./components/screens/CreatePost";
+import { reducer, initialState } from "./reducers/userReducer";
+import UserProfile from "./components/screens/UserProfile";
+import UserSubscribe from "./components/screens/UserSubscribe";
+import Reset from "./components/screens/Reset";
 import NewPassword from "./components/screens/Newpassword";
-import Chat from "./components/screens/Chat"
+import Chat from "./components/screens/Chat";
+import Footer from "./components/Footer";
 // import BookMark from './components/screens/BookMark';
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
-const Routing = () =>{
-  const history = useHistory()
-  const {state,dispatch} = useContext(UserContext)
-  useEffect(()=>{
-    const user = JSON.parse(localStorage.getItem("user"))
-if(user){
-  dispatch({type:"USER",payload:user})
-}else{
-  if(!history.location.pathname.startsWith("/reset"))
-  history.push('/signin')
-}
-  },[])
+const Routing = () => {
+  const history = useHistory();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "USER", payload: user });
+    } else {
+      if (!history.location.pathname.startsWith("/reset"))
+        history.push("/signin");
+    }
+  }, []);
 
-  return(
-    <Switch>
-<Route exact path="/" >
-      <Home />
-      </Route>
-      <Route path="/signin">
-        <Signin />
-      </Route>
-      <Route path="/signup">
-        <Signup/>
-      </Route>
-      <Route exact path = "/profile">
-        <Profile />
-      </Route>
-      <Route path="/createpost">
-        <CreatePost />
-      </Route>
-      <Route path="/profile/:userid">
-        <UserProfile />
-      </Route>
-      <Route path="/bookMark">
-        {/* <BookMark /> */}
-      </Route>
-      <Route exact path="/reset">
-        <Reset />
-      </Route>
-      <Route path="/reset/:token">
-        <NewPassword />
-      </Route>
-      <Route path="/chat">
-        <Chat />
-      </Route>
-    </Switch>
-  )
-}
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/createpost">
+          <CreatePost />
+        </Route>
+        <Route path="/profile/:userid">
+          <UserProfile />
+        </Route>
+        <Route path="/bookMark">{/* <BookMark /> */}</Route>
+        <Route exact path="/reset">
+          <Reset />
+        </Route>
+        <Route path="/reset/:token">
+          <NewPassword />
+        </Route>
+        <Route path="/chat">
+          <Chat />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
+};
 
 const Admin = () => {
-  return(
+  return (
     <Switch>
       <Route>
         {/* <AppShell path="/dashboard">
           <Dashboard />
         </AppShell> */}
       </Route>
-      </Switch>
-  )
-}
+    </Switch>
+  );
+};
 function App() {
-  const [state,dispatch] = useReducer(reducer,initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <UserContext.Provider value={{state,dispatch}} >
-    <BrowserRouter>
-    <Nav />
-    <Routing />
-    </BrowserRouter>
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Nav />
+        <Routing />
+      </BrowserRouter>
     </UserContext.Provider>
   );
 }
--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,70 +1,83 @@
-import React,{useState,useContext} from 'react'
-import {UserContext} from '../../App'
-import {Link,useHistory} from 'react-router-dom'
-import M from 'materialize-css'
+import React, { useState, useContext } from "react";
+import { UserContext } from "../../App";
+import { Link, useHistory } from "react-router-dom";
+import M from "materialize-css";
 
-const Login = ()=>{
-const {state,dispatch} = useContext(UserContext)
-const history = useHistory();
-const [password,setPassword]= useState("")
-const [email,setEmail]= useState("")
+const Login = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-const PostData = ()=>{ 
-    if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-    M.toast({html:"invalid email"})
-return 
-}   
-
-fetch(`${process.env.REACT_APP_BACKEND_URL
-}/signin`,{
-    method:"post",
-    headers:{
-        "Content-Type":"application/json",
-        "Authorization":"Bearer "+localStorage.getItem('jwt')
-    },
-    body:JSON.stringify({
-        password,
+  const PostData = () => {
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
         email
-    })
-    }).then(res=>res.json())
-    .then(data=>{
-       if(data.error){
-            M.toast({html: data.error })
-        }
-        else{
-            localStorage.setItem('jwt',data.token)
-            localStorage.setItem('user',JSON.stringify(data.user))
-            dispatch({type:"USER",payload:data.user})
-            M.toast({html: "signin success",})
-            history.push('/')  
-        }
-      
-    }).catch(err=>{
-        console.log(process.env.REACT_APP_BACKEND_URL)
-        console.log(err)
-    })
-}
+      )
+    ) {
+      M.toast({ html: "invalid email" });
+      return;
+    }
 
-    return (
-       <div className="mycard">
-           <div className="card auth-card input-field">
-<h2>Instagram</h2>
-<input type="text" 
-placeholder="email"
-value={email}
-onChange={(e)=>setEmail(e.target.value)}
-/>
-<input type="password" 
-placeholder="password" 
-value={password}
-onChange={(e)=>setPassword(e.target.value)}
-/>
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/signin`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({ html: data.error });
+        } else {
+          localStorage.setItem("jwt", data.token);
+          localStorage.setItem("user", JSON.stringify(data.user));
+          dispatch({ type: "USER", payload: data.user });
+          M.toast({ html: "signin success" });
+          history.push("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-<button className="btn waves-effect waves-light #64b5f6 blue lighten-2 " onClick={()=>PostData()}>Login</button>
-<h5><Link to="/signup">You don't have an account ?</Link></h5>
-<h6><Link to="/reset">Forgot Password </Link></h6>
-     </div>
-       </div>
-    )
-} 
-export default Login
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          className="btn waves-effect waves-light #64b5f6 blue lighten-2 "
+          onClick={() => PostData()}
+        >
+          Login
+        </button>
+        <h5>
+          <Link to="/signup">You don't have an account ?</Link>
+        </h5>
+        <h6>
+          <Link to="/reset">Forgot Password </Link>
+        </h6>
+      </div>
+    </div>
+  );
+};
+export default Login;
