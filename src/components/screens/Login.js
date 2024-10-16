12497748a2a838d225d29ec94af4675aa56570e7
Duncanwar <semunda9@gmail.com>
2024-10-16 18:06:58 +0200

    done

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
     }
     // const { data } = await auth.login(email, password);
     // console.log(data);
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/signin`, {
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
       method: "post",
       headers: {
         "Content-Type": "application/json",
@@ -50,40 +50,39 @@ const Login = () => {
   };
 
   return (
-
     <div className="flex min-h-full flex-1 flex-cool justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-  Sign in to your account
-      </h2>
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
       </div>
-        <input className="px-12"
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          />
-        <input
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+      <input
+        className="px-12"
+        type="text"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-        <button
-          className="btn waves-effect waves-light #64b5f6 blue lighten-2 "
-          onClick={() => PostData()}
-        >
-          Login
-        </button>
-        <h5>
-          <Link to="/signup">You don't have an account ?</Link>
-        </h5>
-        <h6>
-          <Link to="/reset">Forgot Password </Link>
-        </h6>
+      <button
+        className="btn waves-effect waves-light #64b5f6 blue lighten-2 "
+        onClick={() => PostData()}
+      >
+        Login
+      </button>
+      <h5>
+        <Link to="/signup">You don't have an account ?</Link>
+      </h5>
+      <h6>
+        <Link to="/reset">Forgot Password </Link>
+      </h6>
     </div>
-
   );
 };
 export default Login;
